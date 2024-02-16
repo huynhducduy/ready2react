b4008c2e1cf9d4d6987068e549aecb8178388dd6
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2024-02-17 00:00:18 +0700

    feat: update typescript config, add legacy browser support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import checker from 'vite-plugin-checker'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+		react(),
+		!process.env.VITEST ? checker({ typescript: true }) : undefined,
+		legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+	],
 })
